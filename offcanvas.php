7498d5f92d216fcc637dd2189dbc9265313eb29a
Josh Benham <joshbenham@gmail.com>
2014-01-29 10:38:15 +1100

    Created a dirty not to use offcanvas navigation pattern

---
+++ offcanvas.php
@@ -0,0 +1,11 @@
+<nav role="navigation" class="navigation navigation--offcanvas">
+
+	<div class="navigation__menu">
+		<?php wp_nav_menu( array(  'theme_location' => 'primary-menu', 'container' => false ) ); ?>
+	</div>
+
+	<a class="navigation__trigger" href="javascript:void(0);">
+		<span>Menu</span>
+	</a>
+
+</nav>
