a23d3959c612e127e558736563f18ac3c845945b
Josh Benham <joshbenham@gmail.com>
2014-01-29 10:55:14 +1100

    Created a dirty not to use offcanvas navigation pattern

---
+++ offcanvas.js
@@ -0,0 +1,8 @@
+;(function($, window, document, undefined) {
+	'use strict';
+
+	$('.navigation--offcanvas .navigation__trigger').on('click', function() {
+		$('body').toggleClass('off-canvas');
+	});
+
+}(jQuery, window, document));
